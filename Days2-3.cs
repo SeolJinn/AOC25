ecdf47e7a2b919aea4f0fcfcdfacc100826c9c65
Alexandru Manolache <jwoo2376@gmail.com>
2025-12-23 21:04:36 +0200

    Day3 Solution

--- Days2-3.cs
+++ Days2-3.cs
@@ -4,6 +4,7 @@ namespace Day2;
 
 class Program
 {
+    // Day 2
     private static bool IsDoubled(string number)
     {
         if (number.Length % 2 != 0) 
@@ -13,9 +14,29 @@ class Program
         return number[..half] == number[half..];
     }
 
+    // Day 3
+    private static bool IsRepeatingPattern(string number)
+    {
+        int length = number.Length;
+        
+        for (int patternLen = 1; patternLen <= length / 2; patternLen++)
+        {
+            if (length % patternLen != 0)
+                continue;
+            
+            string pattern = number[..patternLen];
+            int repeatCount = length / patternLen;
+            
+            if (string.Concat(Enumerable.Repeat(pattern, repeatCount)) == number)
+                return true;
+        }
+        
+        return false;
+    }
+
     static void Main(string[] args)
     {        
-        var input = File.ReadAllText("Day2.txt");
+        var input = File.ReadAllText("Days2-3.txt");
         var ranges = input.Split(",");
 
         long sum = 0;
@@ -27,7 +48,7 @@ class Program
 
             for(long i = number1; i <= number2; i++)
             {
-                if(IsDoubled(i.ToString()))
+                if(IsRepeatingPattern(i.ToString()))
                     sum += i;
             }
         }
