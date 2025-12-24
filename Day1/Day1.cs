0918a8d7c5071182d5efd7a188f4b5e26a950538
Alexandru Manolache <jwoo2376@gmail.com>
2025-12-24 10:48:16 +0200

    Day1 Part 2 Solution

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -7,15 +7,23 @@ class Program
 {
     private static int currentValue = 50;
     private static int Password = 0;
+    private static int Password2 = 0;
 
     private static void Move(string direction, int value)
     {
         switch (direction)
         {
             case "L":
+                if (currentValue == 0)
+                    Password2 += value / 100;
+                else if (value >= currentValue)
+                    Password2 += 1 + (value - currentValue) / 100;
+
                 currentValue = ((currentValue - value) % 100 + 100) % 100;
                 break;
             case "R":
+                Password2 += (currentValue + value) / 100;
+
                 currentValue = (currentValue + value) % 100;
                 break;
         }
@@ -26,7 +34,7 @@ class Program
 
     static void Main(string[] args)
     {
-        
+
         var lines = File.ReadAllLines("Day1.txt");
         foreach (var line in lines)
         {
@@ -35,11 +43,12 @@ class Program
             {
                 var direction = match.Groups[1].Value;
                 var value = int.Parse(match.Groups[2].Value);
-                
+
                 Move(direction, value);
             }
         }
 
         Console.WriteLine($"Password: {Password}");
+        Console.WriteLine($"Password2: {Password2}");
     }
 }
